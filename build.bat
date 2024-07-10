8c16438d96aec9bf89b54219d582b9d30745f02d
ahernandezjr <ahernandezjr0@gmail.com>
2024-07-10 17:21:01 -0500

    0.4.3-2024-07-10: Working on package implementation!     - Added MANIFEST.in, setup.py, and build.bat.     - Updated .gitignore, .gitmodules, .vscode/settings.json, main_ipynb.ipynb, and requirements.txt.     - Moved src/R/.Rhistory and src/R/Project.R to reference/R/.     - Created as submodules nba_pistons_azure_env/ and nba_pistons_databricks_env/ (not in repo).

---
+++ build.bat
@@ -0,0 +1,21 @@
+@echo off
+SETLOCAL
+
+REM Clean up old build artifacts
+echo Cleaning up old build artifacts...
+rmdir /s /q build dist nba_pistons.egg-info
+if %errorlevel% neq 0 (
+    echo Failed to clean up old build artifacts. Exiting.
+    exit /b %errorlevel%
+)
+
+REM Build the package
+echo Building the package...
+python -m build
+if %errorlevel% neq 0 (
+    echo Failed to build the package. Exiting.
+    exit /b %errorlevel%
+)
+
+echo Build process completed successfully.
+ENDLOCAL
